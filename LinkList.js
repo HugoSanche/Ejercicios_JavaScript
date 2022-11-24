82bf9cecead6f412a356d12e72a49205489afe8a
HugoSanche <hbmclaren@gmail.com>
2022-11-23 22:32:16 -0600

    adding in file LinkList the insertion module

--- LinkList.js
+++ LinkList.js
@@ -18,7 +18,6 @@ class LinkedList{
       this.length++;
       return this;
     }
-
     prepend(value){
       const newNode={
         value:value,
@@ -31,18 +30,57 @@ class LinkedList{
       return this;
     }
     printlist(){
-      const array=[];
+      let array=[];
       let currentNode=this.head;
       while (currentNode!== null) {
+        //console.log(currentNode.value);
         array.push(currentNode.value);
         currentNode=currentNode.next;
       }
       return array;
+    }
+
+    insert(index, val){
+      let currentNode=this.head;
+      let preNode={};
+
+      for (let i=0; i<index; i++){
+          preNode=currentNode;
+          //console.log(preNode);
+          currentNode=currentNode.next;
+      }
+
+      let array2=[];
+
+      // while (preNode!== null) {
+      //   console.log(preNode.value);
+      //   //array2.push(preNode.value);
+      //   preNode=preNode.next;
+      // }
+
+      let nextNode=preNode.next;
+      const newNode={
+        value:val,
+        next:nextNode
+      }
+      preNode.next=newNode;
+
+      // console.log(currentNode);
+      // console.log(preNode);
+      // console.log(nextNode);
+      // console.log(newNode);
+      // console.log(currentNode);
+      // console.log(this);
+
+      return this;
+    }
 }
-}
+
 const myLinkedList  = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+
+//console.log(myLinkedList.printlist());
+myLinkedList.insert(2,99);
 console.log(myLinkedList.printlist());
